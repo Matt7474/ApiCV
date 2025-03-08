a4641043f67a822f2f4f8dca39a0ddb40ebe3a8f
Matt7474 <dimier.matt@gmail.com>
2025-03-08 17:31:20 +0100

    test deploy

---
+++ back/db.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+	connectionString: process.env.PG_URL,
+});
+
+pool.on("connect", () => {
+	console.log("Connecté à la base PostgreSQL");
+});
+
+pool.on("error", (err) => {
+	console.error("Erreur inattendue sur un client PostgreSQL inactif", err);
+	process.exit(-1);
+});
+
+export default {
+	query: (text, params) => pool.query(text, params),
+	pool,
+};
--- back/index.js
+++ back/index.js
@@ -6,21 +6,27 @@ import { errorHandler, notFound } from "./src/middlewares/errorHandlers.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./src/config/swaggerConfig.js";
 
+import db from "./db.js";
+
 const app = express();
-cors;
+
+// Test de la connexion à la base de données
+db.pool
+	.connect()
+	.then(() => {
+		console.log("Connecté à la base de données PostgreSQL");
+	})
+	.catch((err) => {
+		console.error("Erreur de connexion à la base de données", err);
+		process.exit(1); // Arrêt du serveur en cas d'échec de la connexion
+	});
+
+// Configuration CORS
 app.use(
 	cors({
 		origin: [
-			"http://localhost:5500",
-			"http://127.0.0.1:5500",
-			"http://localhost:5173",
-			"http://127.0.0.1:5173",
-			"http://localhost:5174",
-			"http://127.0.0.1:5174",
-			"http://localhost:5175",
-			"http://127.0.0.1:5175",
-			"http://localhost:5432",
-			"http://127.0.0.1:5432",
+			"http://localhost:5173", // Frontend React local
+			"http://127.0.0.1:5173", // Frontend React local
 		],
 	}),
 );
@@ -30,19 +36,17 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(express.urlencoded({ extended: false }));
 
+// Documentation Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use("/api", router);
 
-app.use(router);
-
 app.use(notFound);
-
 app.use(errorHandler);
 
 const port = process.env.PORT;
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
 	console.log(`Listening on ${process.env.BASE_URL}:${port}`);
 	console.log(
 		`📄 Documentation Swagger disponible sur http://${process.env.BASE_URL}:${port}/api-docs`,
